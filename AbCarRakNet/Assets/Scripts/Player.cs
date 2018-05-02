b52b5ff1b082f70f8bd17cf4843bf1f807865560
NerbzHub <nathan.nette@gmail.com>
2018-05-02 10:08:54 +1000

    Unity Project and gitignore

---
+++ AbCarRakNet/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float turnRate;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move Forward
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(speed * Vector3.forward * Time.deltaTime);
+
+        // Move Back
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(speed * -Vector3.forward * Time.deltaTime);
+
+        // Turn Right
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(0, turnRate, 0, Space.Self);
+
+        // Turm Left
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(0, -turnRate, 0, Space.Self);
+
+    }
+}
---
+++ AbCarRakNet/Assets/Scripts/scrp_UserInterface.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scrp_UserInterface : MonoBehaviour {
+
+    public Text m_UITimer;
+    private float m_Timer = 90.0f;
+    public Text m_UIPlayer1Score;
+    public Text m_UIPlayer2Score;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_Timer -= Time.deltaTime;
+        m_UITimer.text = "Timer: " + ((int)m_Timer).ToString();
+
+		if(m_Timer < 0)
+		{
+			SceneManager.LoadScene("Level01");
+		}
+
+		//m_UIPlayer1Score.text = "Player 1 Score:";
+		//m_UIPlayer2Score.text = "Player 2 Score:";
+	}
+
+    public void UpdateScore(int team, int points)
+    {
+        switch (team)
+        {
+            case 1:
+                m_UIPlayer1Score.text = "Player 1 Score:" + points;
+                break;
+            case 2:
+                m_UIPlayer2Score.text = "Player 2 Score:" + points;
+                break;
+        }
+    }
+}
